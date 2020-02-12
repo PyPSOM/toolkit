f20c7d902ee600e07a4532fd9e4fd72e7b1536e4
Ryukamusa <helio.mgl@gmail.com>
2020-02-12 22:15:55 +0000

    Fixed path to windows with os.path.join and added docker api

--- docker/run_docker.py
+++ docker/run_docker.py
@@ -1,6 +1,8 @@
+
 import argparse
 import os
 import subprocess
+import docker
 
 parser = argparse.ArgumentParser()
 
@@ -8,17 +10,19 @@ parser.add_argument('--folder', type=str, help='The location of the PyPSOM folde
 parser.add_argument('--command', type=str, help='The location of the PyPSOM folder. If not passed assumes it is on it')
 args = parser.parse_args()
 
+client = docker.from_env()
+
 if __name__ == "__main__":
 
     folder = args.folder if args.folder != None else os.getcwd()
+    volumes = {os.path.join(folder, 'model'): {'bind': '/usr/tmp/pypsom', 'mode': 'rw'}}
     docker_cmd = args.command
 
     if docker_cmd.split(' ')[0] not in ["compile.sh", "configure.sh", "clean.sh"]:
-        command_docker = f'docker run -v {folder}/model:/usr/tmp/pypsom hmalmeida/pypsom:0.63.0 -c "{docker_cmd}"'
+        command_docker = f'-c "{docker_cmd}"'
     else:
-        command_docker = f'docker run -v {folder}/model:/usr/tmp/pypsom hmalmeida/pypsom:0.63.0 tools/{docker_cmd}'
-    print(docker_cmd)
-    ## For degub, delete print after finished
+        command_docker = f'tools/{docker_cmd}'
     
-    print("########################################", command_docker, "########################################", sep = '\n')
-    subprocess.call(command_docker, shell = True)
+    output = client.containers.run('hmalmeida/pypsom:0.63.0', command_docker, volumes = volumes, remove = True)
+
+    print(output.decode('UTF-8').strip())
